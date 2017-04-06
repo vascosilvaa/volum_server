12ec3ea9017e420f359b7e0133709d910b723163
Pedro Araújo <pedroaraujo@ua.pt>
2017-04-06 01:11:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use('/', express.static(path.join(__dirname, 'public/dist')));
+app.use('/app', express.static(path.join(__dirname, 'public/dist')));
 app.use('/api', express.static(path.join(__dirname, 'docs')))
 app.use('/teste', express.static(path.join(__dirname, 'testes')))
 
