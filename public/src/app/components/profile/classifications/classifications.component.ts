f2f6f164b75fc28b70be5a4f0bbfa5e37f27e22d
vascosilvaa <vascosilvaa@gmail.com>
2017-07-05 04:16:51 +0100

    blabla

--- public/src/app/components/profile/classifications/classifications.component.ts
+++ public/src/app/components/profile/classifications/classifications.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef} from '@angular/core';
+import { Overlay } from 'angular2-modal';
 
 @Component({
   selector: 'app-classifications',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassificationsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public overlay: Overlay, vcRef: ViewContainerRef) {
+    overlay.defaultViewContainer = vcRef;
+  }
 
   ngOnInit() {
   }
